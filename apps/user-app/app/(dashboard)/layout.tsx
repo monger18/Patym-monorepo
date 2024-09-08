62945de6cfe33e1ce6565a9435d3496f8fb6038a
monger18 <satyamsri1809@gmail.com>
2024-09-08 09:27:10 +0530

    Seeding the data

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -10,6 +10,7 @@ export default function Layout({
       <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-48">
         <div>
           <SidebarItem href={'/dashboard'} icon={<HomeIcon />} title="Home" />
+
           <SidebarItem
             href={'/transfer'}
             icon={<TransferIcon />}
